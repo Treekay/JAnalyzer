0b656283c21b653bff02d57c4f92c6d70eab9562
gyakkun <xiaowufeixia@gmail.com>
2019-07-17 08:15:43 +0800

    feat: try add cfgGraph field.

--- view/GraphField.java
+++ view/GraphField.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 
@@ -9,9 +10,10 @@ import gui.toolkit.*;
 public class GraphField {
 	public static JTabbedPane contentPane;
     public static JScrollPane astPane;
-    public static JScrollPane cfgPane;
+    public static JPanel cfgPane;
     public static JTextArea astText; // 用于放置抽象语法树
     public static JTextArea cfgText; // 用于放置程序控制流图
+    public static JLabel cfgGraph;
 
     public GraphField() {
     	contentPane = new JTabbedPane();
@@ -22,12 +24,18 @@ public class GraphField {
         astText.setEditable(false);
         astPane = new JScrollPane(astText);
 
+        cfgGraph = new JLabel();
+        cfgGraph.setIcon(new ImageIcon("src/view/Pigeons.jpg"));
+
         cfgText = new JTextArea();
         cfgText.setEditable(false);
-        cfgPane = new JScrollPane(cfgText);
-        
+
+        cfgPane = new JPanel();
+        cfgPane.add(cfgText);
+
         contentPane.addTab("抽象语法树", astPane);
         contentPane.addTab("控制流图", cfgPane);
+        contentPane.addTab("cfgGraph", cfgGraph);
     }
 
     public JTabbedPane getContentPane() {
